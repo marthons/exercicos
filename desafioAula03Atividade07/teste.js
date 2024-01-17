de81932978a99e6675cac32c3e7f75ec313a8d1b
Thon Marchetti <tonmarchetti.im@gmail.com>
2024-01-17 16:58:46 -0300

    Aula03

---
+++ desafioAula03Atividade07/teste.js
@@ -0,0 +1,26 @@
+alert('bem vindo');
+let contador = 1;
+
+while (contador < 10) {
+    alert(contador);
+    contador++;
+}
+
+while (contador != 0) {
+    alert(contador);
+    contador--;
+}
+
+let num = prompt('Digite um número');
+
+while (num != 0){
+    alert(num);
+    num--;
+}
+
+let num2 = prompt('Digite um número');
+
+while (num2 <= 10){
+    alert(num2);
+    num2++;
+}
--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,10 +1,19 @@
-alert ('Olá! eu sou o Marthons');
+alert('Olá! eu sou o Marthons');
 let numeroSecreto = 9;
 console.log(numeroSecreto);
-let chute = prompt ('escolha um numero entre 1 e 10');
+let chute;
+let tentativas = 1;
 
-if(numeroSecreto == chute){
-    console.log(`acertou o nuero secreto ${numeroSecreto}`);
-}else {
-    alert ('errou');
+while (chute != numeroSecreto) {
+    chute = prompt('escolha um numero entre 1 e 10');
+    if (chute == numeroSecreto) {
+        console.log(`acertou o nuero secreto ${numeroSecreto} com ${tentativas} tentativas`);
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O numero secreto é menor que ${chute}`);
+        } else {
+            alert(`O numero secreto é maior que ${chute}`);
+        }
+        tentativas++;
+    }
 }
